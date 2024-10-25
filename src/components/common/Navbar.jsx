24896ee252faac47b0e3f195f51a782d6ffb582c
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-25 22:20:34 +0530

    Found modified or deleted file,  src/components/common/Navbar.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import "../../images/search-logo.png";
 import TotalVisitors from "../Statistics/global-stats/TotalVisitors";
-import TotalPointsMined from "../Statistics/global-stats/TotalPointsMined";
+import TotalPointsMined from "../Statistics/global-stats/PointsMined";
 
 function Navbar() {
   return (
     <div>
-      <nav className="bg-violet fs-3 text-light fw-100">
-        <div className="container-fluid">
-          <p className="text-uppercase">BiNg Search Automator</p>
-          <TotalVisitors />
-          <TotalPointsMined />
+      <nav className="navbar bg-violet text-light fw-100 p-2">
+        <div className="container-fluid p-0">
+          <p className="small d-inline-block">Bing Search Automator</p>
+          <div className="nav-stats">
+            <p className="small d-block">
+              Total Visitors{" "}
+              <p className="d-inline-block">
+                <TotalVisitors />
+              </p>
+            </p>
+            <p className="small d-block">
+              Total Points Mined{" "}
+              <p className="small d-inline-block">
+                <TotalPointsMined />
+              </p>
+            </p>
+          </div>
         </div>
       </nav>
     </div>
