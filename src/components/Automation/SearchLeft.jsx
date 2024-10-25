efb98e30b8b2a8614f0158dd4c2d6bc5c8770055
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-25 22:54:48 +0530

    Found modified or deleted file,  src/components/Automation/SearchLeft.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/Automation/SearchLeft.jsx
+++ src/components/Automation/SearchLeft.jsx
@@ -1,31 +1,9 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 
 function SearchLeft({ settings }) {
-  const [countDown, setCountDown] = useState(settings.delay);
-  const intervalIdRef = useRef(null);
-
-  useEffect(() => {
-    if (countDown > 0) {
-      intervalIdRef.current = setInterval(() => {
-        setCountDown((prevCount) => prevCount - 1);
-      }, 1000);
-    }
-
-    return () => {
-      if (intervalIdRef.current) {
-        clearInterval(intervalIdRef.current);
-      }
-    };
-  }, [countDown]);
-
-  useEffect(() => {
-    setCountDown(settings.delay);
-  }, [settings.delay]);
-
   return (
-    <div>
-      <p>Number of Searches Left: {settings.searchLeft}</p>
-      <p>Next Search In {countDown} sec</p>
+    <div className="">
+      <p className="">Searches Left: {settings.searchLeft}</p>
     </div>
   );
 }
