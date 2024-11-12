3be73a84c0517b989685d870af1a265a3d7e285f
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-12 11:53:47 +0530

    Found modified or deleted file,  src/components/common/Navbar.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,30 +1,8 @@
-import React from "react";
-import "../../images/search-logo.png";
-import TotalVisitors from "../Statistics/global-stats/total-visitors";
-import TotalPointsMined from "../Statistics/global-stats/points-mined";
-
 function Navbar() {
   return (
     <div>
-      <nav className="navbar bg-violet text-light fw-100 p-2">
-        <div className="container-fluid p-0">
-          <p className="small d-inline-block">Bing Search Automator</p>
-          <div className="nav-stats">
-            <p className="small d-block">
-              Total Visitors{" "}
-              <p className="d-inline-block">
-                <TotalVisitors />
-              </p>
-            </p>
-            <p className="small d-block">
-              Total Points Mined{" "}
-              <p className="small d-inline-block">
-                <TotalPointsMined />
-              </p>
-            </p>
-          </div>
-        </div>
-      </nav>
+      <nav className="">
+        <h1 className="main-title text-violet fw-100 display-1 text-capitalize text-center py-3 py-md-5">Bing Rewards Point Miner</h1></nav>
     </div>
   );
 }
