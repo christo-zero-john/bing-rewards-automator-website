2b83bdb6fa9a504e8388d48b0befe8493a90f29e
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-12 19:48:54 +0530

    Found new file,  src/components/user-stats/user-stats.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/components/user-stats/user-stats.jsx
@@ -0,0 +1,45 @@
+import GlobalStatsCard from "./stats-card";
+import deviceImage from "../../images/devices-count.png";
+import automationsImage from "../../images/automations-count.png";
+import searchesImage from "../../images/searches-count.png";
+import pointsMinedImage from "../../images/start-automation.png";
+import { useEffect } from "react";
+
+function UserStats() {
+  let count = {
+    deviceCount: 210,
+    automationsCount: 2100,
+    searchesCount: 2100,
+    pointsMined: 2100,
+  };
+
+  return (
+    <div className="d-flex flex-row overflow-auto no-scrollbar">
+      <GlobalStatsCard
+        title="devices"
+        image={deviceImage}
+        count={count.deviceCount}
+      />
+
+      <GlobalStatsCard
+        title="automations"
+        image={automationsImage}
+        count={count.automationsCount}
+      />
+
+      <GlobalStatsCard
+        title="searches"
+        image={searchesImage}
+        count={count.searchesCount}
+      />
+
+      <GlobalStatsCard
+        title="pointsMined"
+        image={pointsMinedImage}
+        count={count.pointsMined}
+      />
+    </div>
+  );
+}
+
+export default UserStats;
