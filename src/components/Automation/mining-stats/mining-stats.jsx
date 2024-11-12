7260cf3dbd019ca3007eef59ca51b001c8d238e3
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-12 19:48:53 +0530

    Found modified or deleted file,  src/components/Automation/mining-stats/mining-stats.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/Automation/mining-stats/mining-stats.jsx
+++ src/components/Automation/mining-stats/mining-stats.jsx
@@ -1,5 +1,23 @@
-function MiningStats() {
-  return <div></div>;
+import { useState } from "react";
+import NextSearchIn from "./next-search-in";
+
+function MiningStats({ settings, handleSearchComplete }) {
+  const [timeLeft, setTimeLeft] = useState(0);
+  function calculateEstimatedTime() {
+    return "0 mins 0 secs";
+  }
+
+  return (
+      <div className="col-11 col-md-5 mt-4">
+          <h2 className="text-center fw-100">Mining</h2>
+      <NextSearchIn
+        settings={settings}
+        onSearchComplete={handleSearchComplete}
+      />
+      <p className="">Search Left: {settings.searchLeft}</p>
+      <p className="">Estimated Time: {calculateEstimatedTime()}</p>
+    </div>
+  );
 }
 
 export default MiningStats;
