d85e5e4378778b501276ffefa80ae17683a516d2
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-12 11:53:50 +0530

    Found new file,  src/components/visitors-count/visitors.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/components/visitors-count/visitors.jsx
@@ -0,0 +1,13 @@
+import AllTimeVisitors from "./all-time";
+import DailyVisitors from "./daily-visitors";
+
+function Visitors() {
+  return (
+    <div>
+      <AllTimeVisitors />
+      <DailyVisitors />
+    </div>
+  );
+}
+
+export default Visitors;
