d800dbbccc13f1200359e103299ab3c2602fe03c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-12 11:53:46 +0530

    Found modified or deleted file,  src/App.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useRef, useState } from "react";
 import "./App.css";
+import Navbar from "./components/common/Navbar";
 import Automation from "./components/automation/automation";
 import Stats from "./components/statistics/stats";
+import Visitors from "./components/visitors-count/visitors";
+import GlobalStats from "./components/global-stats/global-stats";
 
 function App() {
   // localStorage.clear();
   const iframeRef = useRef(null);
   const [automationStatus, setAutomationStatus] = useState(false);
   return (
-    <div className="text-uppercase">
+    <div className="bg-dark text-light text-uppercase en-Oxanium">
+      <Navbar />
+      <Visitors />
+      <GlobalStats />
       <Automation
         automationStatus={automationStatus}
         setAutomationStatus={setAutomationStatus}
@@ -23,7 +29,11 @@ function App() {
 
         <label className="m-3 fs-5" id="showSearchResults">
           Show Search Results
-          <input type="checkbox" className="m-2 checkbox" id="showSearchResults" />
+          <input
+            type="checkbox"
+            className="m-2 checkbox"
+            id="showSearchResults"
+          />
         </label>
       </div>
 
