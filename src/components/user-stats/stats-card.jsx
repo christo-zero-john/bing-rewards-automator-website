49c2e1ecb7c3a7359ae2285b1e78185453fb0434
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-12 19:48:54 +0530

    Found new file,  src/components/user-stats/stats-card.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/components/user-stats/stats-card.jsx
@@ -0,0 +1,13 @@
+function GlobalStatsCard({ title, image, count }) {
+  return (
+    <div className="global-stats-card w-fit d-inline-block column">
+      <section className="text-center d-inline-block">
+        <img src={image} alt={title + " Count Image"} className="icons-main" />
+        <p className="">{title}</p>
+      </section>
+      <p className="count fw-100 d-inline-block">{count}</p>
+    </div>
+  );
+}
+
+export default GlobalStatsCard;
