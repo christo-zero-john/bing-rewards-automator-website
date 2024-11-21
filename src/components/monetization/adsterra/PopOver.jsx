4fad3fdeb908243c34f7e58600a6a1c0a7c79af4
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-21 15:45:31 +0530

    Found new file,  src/components/monetization/adsterra/PopOver.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/components/monetization/adsterra/PopOver.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+function PopOver() {
+  useEffect(() => {
+    let script = document.createElement("script");
+    script.src =
+      "//beatforumsubstitute.com/60/1d/a2/601da261a143f5bb54dd9af2e76649df.js";
+    script.type = "text/javascript";
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+  return <div className="no-scrollbar"></div>;
+}
+
+export default PopOver;
