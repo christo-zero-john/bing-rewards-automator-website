82d96246bf3412872b2fe7844802fdd81b5bfc85
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-21 16:02:46 +0530

    Update AppRoutes.jsx

--- AppRoutes.jsx
+++ AppRoutes.jsx
@@ -4,7 +4,7 @@ import About from "./src/components/About";
 import AdPage1 from "./src/components/monetization/ad-pages/AdPage1";
 import Banner320x50 from "./src/components/monetization/adsterra/Banner-320x50";
 import Banner468x60 from "./src/components/monetization/adsterra/Banner-468x60";
-import Banner160x300 from "./src/components/monetization/adsterra/banner160x300";
+import Banner160x300 from "./src/components/monetization/adsterra/Banner160x300";
 import Banner160x600 from "./src/components/monetization/adsterra/Banner160x600";
 import Banner300x250 from "./src/components/monetization/adsterra/Banner300x250";
 import Banner728x90 from "./src/components/monetization/adsterra/Banner728x90";
