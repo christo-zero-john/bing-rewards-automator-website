49381bbea64c0994abd0e6d56233436d0ba9c514
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-21 17:17:15 +0530

    Found modified or deleted file,  src/components/monetization/adsterra/NativeBanner.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/monetization/adsterra/NativeBanner.jsx
+++ src/components/monetization/adsterra/NativeBanner.jsx
@@ -2,21 +2,21 @@ import { useEffect } from "react";
 
 function NativeBanner() {
   useEffect(() => {
-    // Create the script element
+    
     const script = document.createElement("script");
     script.src =
-      "//beatforumsubstitute.com/702b3c3e998058f2e03bcbe99aca826b/invoke.js";
+      "https://beatforumsubstitute.com/702b3c3e998058f2e03bcbe99aca826b/invoke.js";
     script.async = true;
     script.setAttribute("data-cfasync", "false");
 
-    // Append the script to the body
+    
     document.body.appendChild(script);
 
-    // Cleanup the script when the component unmounts
+    
     return () => {
       document.body.removeChild(script);
     };
-  }, []); // Empty dependency array ensures this runs only once
+  }, []); 
 
   return (
     <div
