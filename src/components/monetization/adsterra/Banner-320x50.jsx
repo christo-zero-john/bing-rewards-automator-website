e81b2c7d2aac8856ef7e079d9abb8fdc91f14d79
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-21 15:45:30 +0530

    Found new file,  src/components/monetization/adsterra/Banner-320x50.jsx. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/components/monetization/adsterra/Banner-320x50.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+
+function Banner320x50() {
+  useEffect(() => {
+    const optionsScript = document.createElement("script");
+    optionsScript.type = "text/javascript";
+    optionsScript.text = `
+        atOptions = {
+          'key': '2ffca080aef63c2f902b566fca65a7e6',
+          'format': 'iframe',
+          'height': 50,
+          'width': 320,
+          'params': {}
+        };
+      `;
+    document.body.appendChild(optionsScript);
+
+    const invokeScript = document.createElement("script");
+    invokeScript.type = "text/javascript";
+    invokeScript.src =
+      "//beatforumsubstitute.com/2ffca080aef63c2f902b566fca65a7e6/invoke.js";
+    document.body.appendChild(invokeScript);
+
+    return () => {
+      document.body.removeChild(optionsScript);
+      document.body.removeChild(invokeScript);
+    };
+  }, []);
+
+  return <div className="no-scrollbar">{/* Ad content will load here */}</div>;
+}
+
+export default Banner320x50;
