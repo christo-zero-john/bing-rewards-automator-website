aa80fbdb00c0ec88343a7dcc8a233d373549c6ba
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-22 22:03:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import putSettings from "./localstore/put-settings";
 import { Analytics } from "@vercel/analytics/react";
 
 import AdDisplayOffcanvas from "./components/monetization/ad-pages/AdDisplayOffcanvas";
+import Loading from "./components/loading/initial-loading";
 
 function App() {
   // localStorage.clear();
@@ -42,80 +43,64 @@ function App() {
   });
 
   return (
-    <div className="bg-dark text-light text-uppercase en-Oxanium">
+    <div className="bg-dark text-light text-uppercase en-Oxanium position-relative min-vh-100">
+      <Loading />
       <AdDisplayOffcanvas
         adDisplayOffcanvasRef={adDisplayOffcanvasRef}
         settings={settings}
         bool={bool}
       />
-      <Analytics />
-      <Navbar />
-      <Visitors />
-      <GlobalStats />
-      <Automation
-        iframeRef={iframeRef}
-        bool={bool}
-        setBool={setBool}
-        settings={settings}
-        updateSettings={updateSettings}
-        showAdOffcanvas={showAdOffcanvas}
-        hideAdOffcanvas={hideAdOffcanvas}
-      />
-      {/* <MiningStats settings={settings} bool={bool} /> */}
-      <div className="checkboxes">
-        <label className="m-3 fs-5 fw-100" htmlFor="showStatsCheckbox">
-          My Stats
-        </label>
-        <input
-          id="showStatsCheckbox"
-          type="checkbox"
-          className="m-2 checkbox"
-          onClick={(e) => {
-            setBool({ ...bool, showMyStats: e.target.checked });
-          }}
+      <div className="content-wrapper">
+        {" "}
+        {/* Add wrapper for main content */}
+        <Analytics />
+        <Navbar />
+        <Visitors />
+        <GlobalStats />
+        <Automation
+          iframeRef={iframeRef}
+          bool={bool}
+          setBool={setBool}
+          settings={settings}
+          updateSettings={updateSettings}
+          showAdOffcanvas={showAdOffcanvas}
+          hideAdOffcanvas={hideAdOffcanvas}
         />
+        <div className="checkboxes">
+          <label className="m-3 fs-5 fw-100" htmlFor="showStatsCheckbox">
+            My Stats
+          </label>
+          <input
+            id="showStatsCheckbox"
+            type="checkbox"
+            className="m-2 checkbox"
+            onClick={(e) => {
+              setBool({ ...bool, showMyStats: e.target.checked });
+            }}
+          />
 
-        <label className="m-3 fs-5 fw-100" htmlFor="showSearchResultCheckBox">
-          Search Results
-        </label>
-        <input
-          id="showSearchResultCheckBox"
-          type="checkbox"
-          className="m-2 checkbox"
-          onClick={(e) => {
-            setBool({ ...bool, showSearchResult: e.target.checked });
-          }}
-        />
-        <p className="text-center">SHOW LOGS and know whats happening</p>
+          <label className="m-3 fs-5 fw-100" htmlFor="showSearchResultCheckBox">
+            Search Results
+          </label>
+          <input
+            id="showSearchResultCheckBox"
+            type="checkbox"
+            className="m-2 checkbox"
+            onClick={(e) => {
+              setBool({ ...bool, showSearchResult: e.target.checked });
+            }}
+          />
+          <p className="text-center">SHOW LOGS and know whats happening</p>
+        </div>
+        {bool.showMyStats && <UserStats />}
+        <iframe
+          className={`${
+            !bool.showSearchResult && "hidden"
+          } webView w-100 no-scrollbar`}
+          ref={iframeRef}
+          src="https://www.bing.com/search?FORM=U523DF&PC=U523&q=bing-rewards-automator-website.vercel.app"
+        ></iframe>
       </div>
-      {bool.showMyStats && <UserStats />}
-      <iframe
-        src="/ads/adsterra/banner-728-90"
-        className="banner-728-90 no-scrollbar"
-      ></iframe>
-      <iframe
-        className={`${
-          !bool.showSearchResult && "hidden"
-        } webView w-100 no-scrollbar`}
-        ref={iframeRef}
-        src="https://www.bing.com/search?FORM=U523DF&PC=U523&q=bing-rewards-automator-website.vercel.app"
-      ></iframe>
-      <iframe
-        src="/ads/adsterra/banner-728-90"
-        className="banner-728-90 no-scrollbar"
-      ></iframe>{" "}
-      <iframe
-        src="/ads/adsterra/banner-728-90"
-        className="banner-728-90 no-scrollbar"
-      ></iframe>{" "}
-      <iframe
-        src="/ads/adsterra/banner-728-90"
-        className="banner-728-90 no-scrollbar"
-      ></iframe>{" "}
-      <iframe
-        src="/ads/adsterra/banner-728-90"
-        className="banner-728-90 no-scrollbar"
-      ></iframe>
     </div>
   );
 }
