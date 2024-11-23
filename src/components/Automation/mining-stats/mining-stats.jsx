e71bf136abd0464c7baee41da1c7538c7f59c31e
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-11-23 17:57:19 +0530

    Found modified or deleted file,  src/components/Automation/mining-stats/mining-stats.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/Automation/mining-stats/mining-stats.jsx
+++ src/components/Automation/mining-stats/mining-stats.jsx
@@ -14,7 +14,7 @@ function MiningStats({ settings, bool }) {
 
   useEffect(() => {
     estimatedTime = calculateEstimatedTime();
-  }, [settings.searchLeft]);
+  }, []);
 
   useEffect(() => {
     if (bool.is_automating) {
@@ -38,16 +38,16 @@ function MiningStats({ settings, bool }) {
   }, [bool.is_automating]);
 
   return (
-    <div className="col-11 col-md-5 mining-stats">
-      <h2 className="text-center fw-100 bg-dark mining-title d-inline-block">
+    <div className="col-11 col-md-5 mining-stats text-nowrap">
+      <h2 className="text-center fs-5 fw-100 bg-dark mining-title d-inline-block">
         Mining
       </h2>
       <NextSearchIn settings={settings} bool={bool} />
-      <div className="">
-        <p className="d-inline-block px-3">
+      <div className="d-inline-block">
+        <p className="d-inline-block pe-3">
           Search Left: {settings.searchLeft}
         </p>
-        <p className="d-inline-block px-3" ref={estimatedTimeRef}>
+        <p className="d-inline-block pe-3" ref={estimatedTimeRef}>
           Estimated Time:{" "}
           <span className="text-orange">{estimatedTime[0]} mins </span>
           <span className="text-aqua">{estimatedTime[1]} secs</span>
